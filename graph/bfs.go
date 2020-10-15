62397d439e7c4a6824b60383d013a6a73d8fe4da
cnbailian <594647004@qq.com>
2020-10-15 10:37:34 +0800

    Add BFS

---
+++ graph/bfs.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package graph
+
+import "sync"
+
+type queue struct {
+	list []string
+	lock sync.Mutex
+}
+
+func newQueue() *queue {
+	return &queue{
+		lock: sync.Mutex{},
+	}
+}
+
+func (q *queue) Push(el string) {
+	q.lock.Lock()
+	q.list = append(q.list, el)
+	q.lock.Unlock()
+}
+
+func (q *queue) Pop() string {
+	if len(q.list) == 0 {
+		return ""
+	}
+	q.lock.Lock()
+	el := q.list[0]
+	q.list = q.list[1:]
+	q.lock.Unlock()
+	return el
+}
+
+func BFSAdjacencyTable(graph AdjacencyTable, root string, end string) bool {
+	queue := newQueue()
+	if _, ok := graph[root]; !ok {
+		return false
+	}
+	if _, ok := graph[end]; !ok {
+		return false
+	}
+	for _, vertex := range graph[root] {
+		queue.Push(vertex)
+	}
+	for i := 0; i < len(graph); i++ {
+		vertex := queue.Pop()
+		if vertex == end {
+			return true
+		}
+		for _, v := range graph[vertex] {
+			queue.Push(v)
+		}
+	}
+	return false
+}
---
+++ graph/bfs_test.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package graph
+
+import "testing"
+
+func TestBFSAdjacencyTable(t *testing.T) {
+	var validate = []struct {
+		start    string
+		end      string
+		expected bool
+	}{
+		{"you", "thom", true},
+		{"you", "anuj", true},
+		{"bob", "claire", false},
+		{"you", "Nonexistent", false},
+	}
+	for _, v := range validate {
+		result := BFSAdjacencyTable(at, v.start, v.end)
+		if result != v.expected {
+			t.Errorf("start: %s, end: %s, result: %t, expected: %t", v.start, v.end, result, v.expected)
+		}
+	}
+}
+
+func BenchmarkBFSAdjacencyTable(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BFSAdjacencyTable(at, "you", "thom")
+	}
+}
