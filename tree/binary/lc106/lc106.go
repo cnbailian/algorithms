094251b3d265b7e2374531f118820a94b439e8e8
cnbailian <594647004@qq.com>
2020-10-30 11:04:41 +0800

    leetcode 106

--- tree/binary/lc106/lc106.go
+++ tree/binary/lc106/lc106.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package lc105
+package lc106
 
 /**
  * Definition for a binary tree node.
@@ -31,25 +31,26 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// 105. 从前序与中序遍历序列构造二叉树
-// 前序遍历时第一个一定是根节点，通过根节点在中序遍历中确定左树和右树
-func buildTree(preorder []int, inorder []int) *TreeNode {
-	if len(preorder) == 0 {
+// 106. 从中序与后序遍历序列构造二叉树
+// 思路与 105 一样，后序遍历最后一个是根节点，根据根节点在中序遍历中寻找左树和右树
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(postorder) == 0 {
 		return nil
 	}
-	// 根节点总是前序排序的第一个
-	root := &TreeNode{Val: preorder[0]}
-	if len(preorder) == 1 {
+	// 根节点总是后序排序的最后一个
+	root := &TreeNode{Val: postorder[len(postorder)-1]}
+	if len(postorder) == 1 {
 		return root
 	}
+
 	// 根据根节点取得在中序排序的索引
-	index := getRootIndex(inorder, preorder[0])
+	index := getRootIndex(inorder, postorder[len(postorder)-1])
 	// 根据索引获得左侧节点和右侧节点
 	inorderLeft := inorder[:index]
 	inorderRight := inorder[index+1:]
-	// 根据节点数量拆分前序排序
-	root.Left = buildTree(preorder[1:len(inorderLeft)+1], inorderLeft)
-	root.Right = buildTree(preorder[len(inorderLeft)+1:], inorderRight)
+	// 根据节点数量拆分后序排序
+	root.Left = buildTree(inorderLeft, postorder[:len(inorderLeft)])
+	root.Right = buildTree(inorderRight, postorder[len(inorderLeft):len(postorder)-1])
 	return root
 }
 
