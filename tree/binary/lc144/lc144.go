9cfb83f20aac765d005f8a4da52e11b3a29eace2
cnbailian <594647004@qq.com>
2020-11-04 10:43:32 +0800

    leetcode 144 二叉树的前序遍历迭代实现

--- tree/binary/lc144/lc144.go
+++ tree/binary/lc144/lc144.go
@@ -45,3 +45,40 @@ func preorderTraversal(root *TreeNode) (vals []int) {
 	preorder(root)
 	return vals
 }
+
+// 利用栈迭代
+type stack struct {
+	Data []*TreeNode
+}
+
+func (s *stack) Pop() *TreeNode {
+	res := s.Data[len(s.Data)-1]
+	s.Data = s.Data[:len(s.Data)-1]
+	return res
+}
+
+func (s *stack) Add(data *TreeNode) {
+	s.Data = append(s.Data, data)
+}
+
+func (s *stack) Len() int {
+	return len(s.Data)
+}
+
+func preorderTraversalStack(root *TreeNode) (res []int) {
+	stack := stack{}
+	// 前序遍历从根开始，所以直接放入
+	stack.Add(root)
+	for stack.Len() > 0 {
+		// 出栈，执行前序遍历逻辑
+		root = stack.Pop()
+		if root == nil {
+			continue
+		}
+		res = append(res, root.Val)
+		// 因为栈先入后出的特性，这里要先放入 right，才能正确的执行前序遍历 根、左、右的顺序
+		stack.Add(root.Right)
+		stack.Add(root.Left)
+	}
+	return
+}
