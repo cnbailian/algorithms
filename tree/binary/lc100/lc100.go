cbce509fbf172fd3b4db291d15103e1ef281ba87
cnbailian <594647004@qq.com>
2020-11-06 13:45:17 +0800

    leetcode 100 相同的树

---
+++ tree/binary/lc100/lc100.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package lc100
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 100. 相同的树
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	// case1
+	if p == nil && q == nil {
+		return true
+	}
+	// case2 节点不统一
+	if p == nil || q == nil {
+		return false
+	}
+	// case3 节点值不统一
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
