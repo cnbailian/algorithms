008e767b230f6c43f3f03014eb7548e8401ea2b1
cnbailian <594647004@qq.com>
2020-11-10 09:35:13 +0800

    leetcode 235 二叉搜索树的最近公共祖先

---
+++ tree/binary/search/lc235/lc235.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package lc235
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 235. 二叉搜索树的最近公共祖先
+// 应用二叉搜索树的性质，左侧一定比根小，右侧一定比根大
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	return root
+}
