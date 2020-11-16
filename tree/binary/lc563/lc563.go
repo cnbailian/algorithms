2160926ec02e4f2a1f1b35dee827c78b679cfcd3
cnbailian <594647004@qq.com>
2020-11-16 08:56:52 +0800

    leetcode 563. 二叉树的坡度

---
+++ tree/binary/lc563/lc563.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package lc563
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 563. 二叉树的坡度
+func findTilt(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return abs(sum(root.Left)-sum(root.Right)) + findTilt(root.Left) + findTilt(root.Right)
+}
+
+func sum(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return root.Val + sum(root.Left) + sum(root.Right)
+}
+
+func abs(i int) int {
+	return int(math.Abs(float64(i)))
+}
