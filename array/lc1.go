92a504adc32b7077ab740188e937afc4d87058f0
cnbailian <594647004@qq.com>
2020-12-16 16:04:09 +0800

    leetcode 1.两数之和

---
+++ array/lc1.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2020 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package array
+
+func twoSum(nums []int, target int) []int {
+	for i, num := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if num+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	dVals := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		if p, ok := dVals[target-nums[i]]; ok {
+			return []int{i, p}
+		}
+		dVals[nums[i]] = i
+	}
+	return nil
+}
