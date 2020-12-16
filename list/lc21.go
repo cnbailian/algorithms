c48a6eefaa5e5772f9f62cedd68d8758c155e11c
cnbailian <594647004@qq.com>
2020-12-16 16:05:06 +0800

    leetcode 21.合并两个有序链表

--- list/lc21.go
+++ list/lc21.go
@@ -15,3 +15,45 @@ limitations under the License.
 */
 
 package list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 21. 合并两个有序链表
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{}
+	current := res
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			current.Next = l2
+			l2 = l2.Next
+		} else {
+			current.Next = l1
+			l1 = l1.Next
+		}
+		current = current.Next
+	}
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+	return res.Next
+}
+
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val > l2.Val {
+		l2.Next = mergeTwoLists2(l1, l2.Next)
+		return l2
+	}
+	l1.Next = mergeTwoLists2(l1.Next, l2)
+	return l1
+}
